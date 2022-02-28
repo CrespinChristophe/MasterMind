a052622dde30297ae5b3375176d559bdff77874b
CrespinChristophe <crespin.chris@gmail.com>
2022-02-28 23:57:49 +0100

    premier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MastermindComponent } from './mastermind/mastermind.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"", redirectTo: "mastermind", pathMatch: 'full' },
+  { path : "mastermind", component: MastermindComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
