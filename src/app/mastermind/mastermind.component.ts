ca27998f702e6ca11d27f0ed60951bcca799ca39
Crespin <crespin.christophe@netc.eu>
2022-03-01 14:38:12 +0100

    s

--- src/app/mastermind/mastermind.component.ts
+++ src/app/mastermind/mastermind.component.ts
@@ -8,14 +8,84 @@ import { Component, OnInit } from '@angular/core';
 export class MastermindComponent implements OnInit {
 
   compteurCouleurs: number = 0;
-  //bleu: string;
+  varBleu: string = "bleu";
+  varRouge: string = "rouge";
+  varVert: string = "vert";
+  varJaune: string = "jaune";
+  varRose: string = "rose";
+  varNoir: string = "noir";
+  varVertClair: string = "vert clair";
+  couleurChoisie: string ="";
+  //warning: string ="";
+  isAvailable: boolean = false;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
   
+  constructor() { }
+
+  
+
+  ngOnInit(): void {
+
+    /*if ( this.compteurCouleurs == 4) {
+
+      console.log("le nombre de couleurs est égal à 4");
+      
+    } */
+
+  }
+
+  essais(): void {
+    if ( this.compteurCouleurs == 4) {
+
+      //this.warning="Le nombre de couleurs choisie est atteint !!!!";
+      this.isAvailable = true;
+      
+    } 
+  }
+
+  bleu(): void {
+    this.compteurCouleurs++;
+    this.couleurChoisie = "bleu";
+    this.essais();
+  }
+
+  rouge(): void {
+    this.compteurCouleurs++;
+    this.couleurChoisie = "rouge";
+    this.essais();
+  }
+
+  vert(): void {
+    this.compteurCouleurs++;
+    this.couleurChoisie = "vert";
+    this.essais();
+  }
+
+  jaune(): void {
+    this.compteurCouleurs++;
+    this.couleurChoisie = "jaune";
+    this.essais();
+  }
+
+  rose(): void {
+    this.compteurCouleurs++;
+    this.couleurChoisie = "rose";
+    this.essais();
+  }
+
+  noir(): void {
+    this.compteurCouleurs++;
+    this.couleurChoisie = "noir";
+    this.essais();
+  }
+
+  vertclair(): void {
+    this.compteurCouleurs++;
+    this.couleurChoisie = "vert clair";
+    this.essais();
+  }
 
   set(): void {
     console.log("vous avez cliqué sur set");
@@ -25,5 +95,7 @@ export class MastermindComponent implements OnInit {
     console.log("vous avez cliqué sur reset");
 
   }
+
+  
 
 }
